f2da49960521582210869bcbaffe5c522a7625fc
FrancoSanticchia1 <franco.santicchia03@gmail.com>
2023-04-09 20:19:16 -0300

    carpetas detalles y ver todas

---
+++ src/screens/NotFound/NotFound.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <h1>Error 404 No se han encontrado resultados</h1>
+    )
+  }
+}
