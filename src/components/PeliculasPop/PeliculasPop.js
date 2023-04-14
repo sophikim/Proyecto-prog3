845ddd3dc732aa93392d21f737fa610bfbc9f768
ManuelEspora <mespora@udesa.edu.ar>
2023-04-14 20:48:52 -0300

    Home actualizado

--- src/components/PeliculasPop/PeliculasPop.js
+++ src/components/PeliculasPop/PeliculasPop.js
@@ -73,24 +73,24 @@ class PeliculasPop extends Component {
                 <p className={this.state.clase}>{this.props.datosPeliculas.overview}</p>
               </div>
 
-              <div>
-                {
-                  this.state.esFavorito ?
-                  <button onClick={()=> this.sacarFav(this.props.info.id)}> Sacar de Favoritos</button>
-                  :
-                  <button onClick={()=>this.anhadirFav(this.props.info.id)}> Añadir a Favoritos</button>
-                } 
-                  <button onClick={()=> this.cambiarTexto()}>{this.state.texto}</button>
-                    
-              </div>
+            <div>
+            {
+              this.state.esFavorito ?
+              <button onClick={()=> this.sacarFav(this.props.datosPeliculas.id)}> Sacar de Favoritos</button>
+              :
+              <button onClick={()=>this.anhadirFav(this.props.datosPeliculas.id)}> Añadir a Favoritos</button>
+            } 
+              <button onClick={()=> this.cambiarTexto()}>{this.state.texto}</button>
+                
+            </div>
 
-              <div className='button'>
-                <Link to={`/peliculas/detalle/id/${this.props.datosPeliculas.id}`}>
-                    <button>Ir a detalle</button>
-                </Link>
-              </div>
+            <div className='button'>
+                  <Link to={`/detallespeli/id/${this.props.datosPeliculas.id}`}>
+                      <button>Ir a detalle</button>
+                  </Link>
 
-            
+                   
+              </div>
             </article>
 
             </>
